4c1587c8e6d4be2b7b7771a4bb186c95bee798af
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-01-30 17:05:11 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,11 +22,81 @@ def cleanup_code(content):
     return content.strip('` \n')
 
 @bot.command()
-async def help(self, ctx, cmd=None):
+async def help(ctx):
     color = discord.Color(value=0x00ff00)
     em = discord.Embed(color=color, title='Bot Commands')
     em.description = "**Who am I? I am dat banana boi #1982's simple eval bot. I can help you run Python code.** \n <this> means that the argument is **optional**. \n [this] means that the argument is **required**."
-    em.add_field(name='help <category|command>', value='Shows the help message for this bot.')
+    em.add_field(name='help', value='Shows the help message for this bot.')
     em.add_field(name='eval [code]', value='Runs Python code. Great for testing. Also the main purpose of this bot.')
     em.add_field(name='invite', value='Aye! Invite me to your server.')
+    em.add_field(name='ping', value='PONG! Returns websocket latency.')
+    await ctx.send(embed=em)
+    
+    
+@bot.command()
+async def eval(ctx, code):
+@bot.command(hidden=True, name='eval')
+async def _eval(ctx, *, body: str):
+    env = {
+        'bot': bot,
+        'ctx': ctx,
+        'channel': ctx.channel,
+        'author': ctx.author,
+        'guild': ctx.guild,
+        'message': ctx.message,
+    }
+
+    env.update(globals())
+
+    body = cleanup_code(body)
+    stdout = io.StringIO()
+
+    to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
+
+    try:
+        exec(to_compile, env)
+    except Exception as e:
+        return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
+
+    func = env['func']
+    try:
+        with redirect_stdout(stdout):
+            ret = await func()
+    except Exception as e:
+        value = stdout.getvalue()
+        await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
+    else:
+        value = stdout.getvalue()
+        try:
+            await ctx.message.add_reaction('\u2705')
+        except:
+            pass
+
+        if ret is None:
+            if value:
+                await ctx.send(f'```py\n{value}\n```')
+        else:
+            await ctx.send(f'```py\n{value}{ret}\n```')  
+
+          
+@bot.command()
+async def invite(ctx):
+    await ctx.send("Aye! Lemme join that server: https://discordapp.com/oauth2/authorize?client_id=406882538116743168&scope=bot&permissions=8")
+    
+    
+@bot.command()
+async def ping(ctx):
+    """Premium ping pong giving you a websocket latency."""
+    color = discord.Color(value=0x00ff00)
+    em = discord.Embed(color=color, title='PoIIIng! Your supersonic latency is:')
+    em.description = f"{bot.latency * 1000:.4f} ms"
+    em.set_footer(text="Psst...A heartbeat is 27 ms!")
+    await ctx.send(embed=em)
+    
+    
+if not os.environ.get('TOKEN'):
+    print("no token found REEEE!")
+bot.run(os.environ.get('TOKEN').strip('"'))
+
+    
     
