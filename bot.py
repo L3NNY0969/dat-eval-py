f18cefe6166bafd605f0fd5f0d6faeee3841d6c2
bananaboy21 <33041238+bananaboy21@users.noreply.github.com>
2018-04-02 13:51:04 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -56,8 +56,10 @@ async def help(ctx):
 
 @bot.command(hidden=True, name='eval', aliases=['val'])
 async def _eval(ctx, *, body: str):
-    if "bot.ws.token" in body or "os.environ.get('TOKEN')" in body or 'os.environ.get("TOKEN")' in body:
+    if "bot.ws.token" in body:
         return await ctx.send("I see you have tried to leak my token. Nice try, buddy. :rofl:")
+    if "os.environ" in body:
+        return await ctx.send("You can't access my config vars. Nice try, though.")
     lol = bot.get_channel(408030365773463562)
     await lol.send(f"**{ctx.message.author.name}** has run the code: \n\n```{body}```")        
     env = {
